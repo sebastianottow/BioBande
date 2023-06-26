2011d750bb540374ee61b6956bfd6ab4b81651de
Sebastian Ottow <seb.ottow@gmail.com>
2023-06-26 11:14:11 +0200

    add completionHanlder to string to coord conversion prior to api call

---
+++ AnnotationViewModel.swift
@@ -0,0 +1,89 @@
+//
+//  AnnotationViewModel.swift
+//  FoodMeMVP_SO
+//
+//  Created by Sebastian Ottow on 26.03.23.
+//
+
+import Combine
+import CoreLocation
+import UIKit
+import RealmSwift
+
+
+class AnnotationViewModel: ObservableObject {
+    
+    let realm = try! Realm()
+    
+    let _categoryList = try! Realm().objects(CategoryModel.self)
+
+    let annotationServices = AnnotationServices()
+        
+    var entries = [AnnotationModel]()
+    
+    @Published var street: String = ""
+    @Published var postalCode: String = ""
+    @Published var city: String = ""
+    @Published var longCoord: Double = 0
+    @Published var latCoord: Double = 0.0
+
+    func forwardGeocoding(street: String, postalCode: String, city: String, completion: @escaping (Bool, Error?) -> Void) {
+
+        let address = "\(street) \(postalCode) \(city)"
+
+        let geocoder = CLGeocoder()
+        geocoder.geocodeAddressString(address) { (placemarks, error) in
+            if let error = error {
+                print("Failed to retrieve location: \(error)")
+                completion(false, error)
+                return
+            }
+
+            var location: CLLocation?
+
+            if let placemarks = placemarks, placemarks.count > 0 {
+                location = placemarks.first?.location
+            }
+
+            if let location = location {
+                self.longCoord = location.coordinate.longitude
+                self.latCoord = location.coordinate.latitude
+
+                completion(true, nil)
+            }
+            else
+            {
+                guard let error = error else { return }
+                print("No matching location found: \(error)")
+                completion(false, error)
+            }
+        }
+    }
+}
+
+extension AnnotationViewModel {
+    
+    var isValidStreetPublisher: AnyPublisher<Bool, Never> {
+        $street
+            .map { $0.count > 3 }
+            .eraseToAnyPublisher()
+    }
+    
+    var isValidPostalCodePublisher: AnyPublisher<Bool, Never> {
+        $postalCode
+            .map { $0.count == 5 }
+            .eraseToAnyPublisher()
+    }
+    
+    var isValidCityPublisher: AnyPublisher<Bool, Never> {
+        $city
+            .map { $0.count > 3 }
+            .eraseToAnyPublisher()
+    }
+    
+    var isAddNewEntryButtonEnabled: AnyPublisher<Bool, Never> {
+        Publishers.CombineLatest3(isValidStreetPublisher, isValidPostalCodePublisher, isValidCityPublisher)
+            .map { $0 && $1 && $2 }
+            .eraseToAnyPublisher()
+    }
+}
---
+++ NewAnnotationEntryViewController.swift
@@ -0,0 +1,294 @@
+//
+//  NewAnnotationEntryViewController.swift
+//  FoodMeMVP_SO
+//
+//  Created by Sebastian Ottow on 26.03.23.
+//
+
+import Combine
+import CombineCocoa
+import FirebaseAuth
+import RealmSwift
+import UIKit
+
+import CoreLocation
+
+
+
+class NewAnnotationEntryViewController: UIViewController {
+    
+    private enum Constants {
+        static let defaultIcon = UIImage(systemName: "square.and.pencil")
+        static let dropDownArrowIcon = UIImage(systemName: "arrowtriangle.down.square")
+        
+        static let defaultColor = UIColor.systemIndigo
+    }
+    
+    let realm = try! Realm()
+    
+    private let _categoryList = try! Realm().objects(CategoryModel.self).sorted(byKeyPath: "name", ascending: true)
+    
+    @Published var selectedEntryCategory: String?
+    
+    private let _viewModel = AnnotationViewModel()
+    
+    private var _cancellables: Set<AnyCancellable> = []
+    
+    private let _dismissButton = UIButton()
+    private let _addNewEntryButton = UIButton()
+    
+    private let _dropDownEntryType = CustomDropDown()
+    
+    private let _streetNameTextField: CustomTextField = {
+        let textField = CustomTextField()
+        
+        textField.layer.borderWidth = 1
+        textField.layer.borderColor = UIColor.lightGray.cgColor
+        textField.layer.cornerRadius = 5
+        textField.placeholder = "Straße & Hausnummer..."
+        
+        textField.setIcon(icon: Constants.defaultIcon, color: Constants.defaultColor)
+                
+        return textField
+    }()
+    
+    private let _postalCodeTextField: CustomTextField = {
+        let textField = CustomTextField()
+        
+        textField.layer.borderWidth = 1
+        textField.layer.borderColor = UIColor.lightGray.cgColor
+        textField.layer.cornerRadius = 5
+        textField.placeholder = "Postleitzahl..."
+        
+        textField.setIcon(icon: Constants.defaultIcon, color: Constants.defaultColor)
+        textField.isSelected = true
+        
+        return textField
+    }()
+    
+    private let _cityTextField: CustomTextField = {
+        let textField = CustomTextField()
+        
+        textField.layer.borderWidth = 1
+        textField.layer.borderColor = UIColor.lightGray.cgColor
+        textField.layer.cornerRadius = 5
+        textField.placeholder = "Stadt..."
+        
+        textField.setIcon(icon: Constants.defaultIcon, color: Constants.defaultColor)
+        
+        return textField
+    }()
+    
+    private let _scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        
+        scrollView.showsHorizontalScrollIndicator = false
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return scrollView
+    }()
+    
+    private let _formHolderStackView: UIStackView = {
+        let stackView = UIStackView()
+        
+        stackView.axis = .vertical
+        stackView.distribution = .fill
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.spacing = 10
+        
+        return stackView
+    }()
+    
+    func setupPublishers() {
+        
+        _streetNameTextField.textPublisher
+            .map { "\($0 ?? "")" }
+            .assign(to: \.street , on: _viewModel)
+            .store(in: &_cancellables)
+        
+        _postalCodeTextField.textPublisher
+            .map { "\($0 ?? "")" }
+            .assign(to: \.postalCode, on: _viewModel)
+            .store(in: &_cancellables)
+        
+        _cityTextField.textPublisher
+            .map { "\($0 ?? "")" }
+            .assign(to: \.city, on: _viewModel)
+            .store(in: &_cancellables)
+        
+        _viewModel.isAddNewEntryButtonEnabled
+            .assign(to: \.isEnabled, on: _addNewEntryButton)
+            .store(in: &_cancellables)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupUI()
+        setupPublishers()
+        
+        _dropDownEntryType.$selectedValue
+            .receive(on: DispatchQueue.main)
+            .sink { value in
+                self.selectedEntryCategory = value
+            }
+            .store(in: &_cancellables)
+        
+//        _viewModel.loadEntryCategories()
+    }
+    
+//    private func bind(viewModel: AnnotationViewModel) {
+//        
+//        viewModel.$street
+//            .receive(on: DispatchQueue.main)
+//            .sink { [weak self] street in
+//                self?._streetNameTextField.text = street
+//            }
+//            .store(in: &_cancellables)
+//    }
+    
+    private func setupUI() {
+        view.backgroundColor = .white
+        
+        navigationItem.title = "ADD NEW ENTRY"
+        
+        let navigationBar = navigationController?.navigationBar
+        let navigationBarAppearance = UINavigationBarAppearance()
+        navigationBarAppearance.shadowColor = .darkGray
+        navigationBarAppearance.backgroundColor = Constants.defaultColor
+        navigationBarAppearance.titleTextAttributes = [.foregroundColor: UIColor.systemGreen]
+        navigationBar?.scrollEdgeAppearance = navigationBarAppearance
+        
+        addDismissButton()
+        
+        let closeButton = UIBarButtonItem(customView: _dismissButton)
+        navigationItem.setRightBarButton(closeButton, animated: true)
+        
+        view.addSubview(_scrollView)
+        _scrollView.edgesToSuperview(excluding: [.top, .bottom], insets: .init(top: 0, left: 10, bottom: 0, right: 10))
+        _scrollView.bottomToSuperview(usingSafeArea: true)
+        _scrollView.topToSuperview(usingSafeArea: true)
+        
+        _scrollView.addSubview(_formHolderStackView)
+        _formHolderStackView.edgesToSuperview(excluding: [.bottom], insets: .init(top: 10, left: 0, bottom: 0, right: 0))
+        _formHolderStackView.width(to: _scrollView)
+        
+        _dropDownEntryType.createPickerView()
+        _formHolderStackView.addArrangedSubview(_dropDownEntryType)
+        _dropDownEntryType.selectionList = _categoryList.map { $0.name }
+        _dropDownEntryType.placeholder = "Kategorie auswählen"
+        _dropDownEntryType.setIcon(icon: Constants.dropDownArrowIcon, color: Constants.defaultColor)
+        
+        _formHolderStackView.addArrangedSubview(_streetNameTextField)
+        
+        _formHolderStackView.addArrangedSubview(_postalCodeTextField)
+        _formHolderStackView.addArrangedSubview(_cityTextField)
+        
+        addAddNewEntryButton()
+        _scrollView.addSubview(_addNewEntryButton)
+        _addNewEntryButton.bottomToSuperview(offset: -25, usingSafeArea: true)
+        _addNewEntryButton.edgesToSuperview(excluding: [.top, .bottom], insets: .init(top: 0, left: 10, bottom: 0, right: 10))
+        
+        _scrollView.contentInset = .init(top: 10, left: 0, bottom: 20, right: 0)
+    }
+    
+    private func addAddNewEntryButton() {
+        _addNewEntryButton.configuration = .filled()
+        _addNewEntryButton.configuration?.baseForegroundColor = .white
+        _addNewEntryButton.configuration?.baseBackgroundColor = Constants.defaultColor
+        _addNewEntryButton.setTitle("Add new Entry", for: .normal)
+        _addNewEntryButton.configuration?.cornerStyle = .capsule
+        _addNewEntryButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        _addNewEntryButton.isEnabled = false
+        
+        _addNewEntryButton.addTarget(
+            self,
+            action: #selector(saveNewEntry),
+            for: .touchUpInside
+        )
+    }
+    
+    private func addDismissButton() {
+        _dismissButton.configuration = .filled()
+        _dismissButton.configuration?.baseForegroundColor = .systemGray
+        _dismissButton.configuration?.baseBackgroundColor = .white
+        _dismissButton.configuration?.image = UIImage(
+            systemName: "xmark.circle",
+            withConfiguration: UIImage.SymbolConfiguration(
+                pointSize: 20,
+                weight: .ultraLight
+            )
+        )
+        _dismissButton.configuration?.cornerStyle = .capsule
+        
+        _dismissButton.isEnabled = true
+        
+        _dismissButton.addTarget(
+            self,
+            action: #selector(hideMapDetailViewController),
+            for: .touchUpInside
+        )
+    }
+    
+    private func showToast(controller: UIViewController, message: String, seconds: Double) {
+        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        alert.view.backgroundColor = Constants.defaultColor
+        alert.view.alpha = 0.6
+        alert.view.layer.cornerRadius = 15
+        
+        controller.present(alert, animated: true)
+        
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + seconds) {
+            alert.dismiss(animated: true)
+        }
+    }
+    
+    @objc func saveNewEntry(sender: UIButton) {
+        let entryID = UUID().uuidString
+        guard let userID = Auth.auth().currentUser?.uid, let selectedCategory = selectedEntryCategory else { return }
+
+            _viewModel.forwardGeocoding(
+                        street: _viewModel.street,
+                        postalCode: _viewModel.postalCode,
+                        city: _viewModel.city) { [weak self] wasConverted, error in
+
+                            if let error = error {
+                                print("\(error)")
+                                return
+                            }
+
+                            if wasConverted {
+
+                                let newAnnotationEntry = AnnotationModel(
+                                    userID: userID,
+                                    isPrivate: true,
+                                    entryType: selectedCategory,
+                                    street: self?._viewModel.street ?? "",
+                                    postalCode: self?._viewModel.postalCode ?? "",
+                                    city: self?._viewModel.city ?? "",
+                                    id: entryID,
+                                    longCoord: self?._viewModel.longCoord ?? 0.0,
+                                    latCoord: self?._viewModel.latCoord ?? 0.0
+                                )
+
+                                self?._viewModel.annotationServices.saveAnnotationToDB(with: newAnnotationEntry)
+
+                                self?._dropDownEntryType.text?.removeAll()
+                                self?._streetNameTextField.text?.removeAll()
+                                self?._postalCodeTextField.text?.removeAll()
+                                self?._cityTextField.text?.removeAll()
+
+//                                showToast(controller: self, message: "Entry successfully added!", seconds: 3)
+
+                            } else {
+                                print("irgendwas lief falsch!")
+                            }
+
+                        }
+    }
+    
+    @objc func hideMapDetailViewController(sender: UIButton) {
+        self.dismiss(animated: true)
+    }
+}
